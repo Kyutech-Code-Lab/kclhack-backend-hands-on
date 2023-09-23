ec520acdb27c6ce5de05bf8133ee461b4177c799
claustra01 <shinya430.g@gmail.com>
2023-09-23 16:03:27 +0900

    Add post form

---
+++ src/components/PostFormSample.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import styles from '@/styles/Home.module.css'
+
+export default function PostForm() {
+
+  const [content, setContent] = useState<string>('')
+
+  const submitPost = async () => {
+    try {
+      // POSTリクエストを送信
+      const response = await fetch('/api/sample/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content }),
+      });
+
+      if (response.ok) {
+        // POSTが成功した場合の処理
+        setContent('')
+        console.log('投稿に成功しました！')
+      } else {
+        // POSTが失敗した場合の処理
+        console.error(response.statusText)
+      }
+    } catch (error) {
+      // エラーハンドリング
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className={styles.postform}>
+      <input
+        type="text"
+        placeholder="テキストを入力"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <button onClick={submitPost}>投稿</button>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import PostFormSample from '@/components/PostFormSample'
 
 export default function Home() {
+
+  
+
   return (
     <>
       <Head>
@@ -9,8 +13,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
-        <h1>KCLHack Hands-On</h1>
-        
+        <div className={styles.container}>
+          <h1>KCLHack Hands-On</h1>
+          <a href='/sample'>サンプルページへ移動</a>
+          <PostFormSample />
+        </div>
       </div>
     </>
   )
