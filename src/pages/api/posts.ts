29d54051d80e316427979bdc5058f5911daea355
claustra01 <shinya430.g@gmail.com>
2023-09-24 04:31:15 +0900

    Add error response message

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -10,31 +10,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     // 投稿作成用API
     case 'POST':
-      // まだ実装されておらず，NotImplementedを返している
-      res.status(501).end()
+      // まだ実装されていないため，NotImplementedを返している
+      res.status(501).json({ message: 'this method is not implemented' })
       break
 
     // 投稿取得用API
     case 'GET':
-      // まだ実装されておらず，NotImplementedを返している
-      res.status(501).end()
+      // まだ実装されていないため，NotImplementedを返している
+      res.status(501).json({ message: 'this method is not implemented' })
       break
     
     // 投稿編集用API
     case 'PUT':
-      // まだ実装されておらず，NotImplementedを返している
-      res.status(501).end()
+      // まだ実装されていないため，NotImplementedを返している
+      res.status(501).json({ message: 'this method is not implemented' })
       break
     
     // 投稿削除用API
     case 'DELETE':
-      // まだ実装されておらず，NotImplementedを返している
-      res.status(501).end()
+      // まだ実装されていないため，NotImplementedを返している
+      res.status(501).json({ message: 'this method is not implemented' })
       break
 
     // 意図しないメソッドの場合，MethodNotAllowedを返す
     default:
-      res.status(405).end()
+      res.status(405).json({ message: 'supported methods are POST, GET, PUT and DELETE' })
       break
   
   }
